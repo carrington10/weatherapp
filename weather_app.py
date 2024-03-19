81f7c1f92c7134bed1ed6bcbc7ee691b245a60a6
cooper <carringtonhate@gmail.com>
2024-03-18 23:53:13 -0700

    adding in weather functionality

--- weather_app.py
+++ weather_app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from configparser import ConfigParser
 import requests;
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
@@ -8,6 +9,7 @@ config = ConfigParser();
 config.read(config_file);
 apik = config['api_key']['key'];
 
+# function that makes an apicall to get the weather of the given city 
 def get_weather(city):
     result = requests.get(url.format(city,apik));
     if result:
@@ -20,18 +22,23 @@ def get_weather(city):
         final = (cityN,country,temp_kel,temp_cel,temp_far)
         print(final);
         return final;
-
-
     else:
         return None;
 app = Tk();
 app.title("Weather App");
 app.geometry("700x350");
 
-get_weather('london');
 
 def search():
-    pass;
+    city = city_text.get()
+    print(city)
+    weather = get_weather(city);
+    if weather:
+        location_lnl['text'] = '{},{}'.format(weather[0],weather[1])
+        temp_lbl['text'] = '{:.2f}C, {:.2f}F'.format(weather[2],weather[3]);
+        weather_lbl['text'] = weather[4]
+    else:
+        messagebox.showerror('Error','Cannot find the city {}'.format(city))
 
 city_text = StringVar();
 city_entry = Entry(app,textvariable=city_text)
